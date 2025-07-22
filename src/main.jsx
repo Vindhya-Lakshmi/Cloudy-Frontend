abc37864b42ff8b5d39c9b3dbb0a157771b7759f
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2025-07-22 19:12:51 +0530

    starting

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react' // ✅ FIXED
+import { ChakraProvider } from '@chakra-ui/react' // ✅ FIXED
 
-const customTheme = extendTheme({
-  config: {
-    initialColorMode: 'light',
-    useSystemColorMode: false,
-  },
-})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider theme={customTheme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>,
